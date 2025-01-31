4f3b603ddaccf4c1fd3a50f5251e1bdff54bc539
“[Rayan <[rayan077@academiacollege.edu.np]>
2025-01-31 19:32:53 +0545

    Database and Backend Initializzation

---
+++ HamraKitab.DataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using HamraKitab.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HamraKitab.DataAccess.Data
+{
+    public class ApplicationDbContext : DbContext    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+        public DbSet<Genre> Genres { get; set; }
+    }
+}
